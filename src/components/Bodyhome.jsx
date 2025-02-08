03ba86b2205ddf2d57a67f6fb4e0efead5aebd61
Dilip Vishwakarma <2106dilip@gmail.com>
2025-02-09 01:51:54 +0530

    give style to more product btn and add product card in top product cards

--- src/components/Bodyhome.jsx
+++ src/components/Bodyhome.jsx
@@ -1,26 +1,57 @@
 import React from "react";
 import products from "../assets/api/topProducts.json";
 import { NavLink } from "react-router-dom";
+import { Heart } from "react-feather";
 
 const Bodyhome = () => {
   return (
-
     // main body
-    <div className=" w-full h-screen flex justify-center"> 
-
-        {/*product Container  */}
+    <div className=" w-full h-screen flex justify-center">
+      {/*product Container  */}
       <div className=" w-4/5 h-full flex gap-8 py-8">
-            {/* product line box */}
+        {/* product line box */}
         <div className=" w-2/5 flex flex-col justify-center gap-20">
-          <h1 className=" text-[4.6rem] font-normal">Perfect Match Every New Occasion</h1>
+          <h1 className=" text-[4.6rem] font-normal">
+            Perfect Match Every New Occasion
+          </h1>
           <NavLink to="/products">
-            <button className=" bg-buttonColor text-base w-36 h-12 rounded-lg border-2 border-black hover:shadow-boxShadow">More Products</button>
+            <button className=" bg-buttonColor text-base w-36 h-12 rounded-lg border-2 border-black hover:shadow-boxShadow active:bg-clickColor shadow-none">
+              More Products
+            </button>
           </NavLink>
         </div>
 
-        <div class="topProducts">{products.forEach((curProd) => {
-            
-        })}</div>
+        {/* top products */}
+        <div className=" w-3/5 flex items-center justify-around">
+          {products.map((curProd) => {
+            const { id, name, price, image } = curProd;
+
+            return (
+              // productCard
+              <div
+                className=" h-[70%] w-80 bg-white p-4 rounded-2xl shadow-boxShadow flex flex-col gap-[0.8rem]"
+                id={id}
+                key={id}
+              >
+                <div className=" rounded-tl-2xl rounded-tr-2xl overflow-hidden relative group">
+                  <img
+                    src={image}
+                    alt={name}
+                    className="group-hover:scale-[1.2] transition-all h-72 ease-linear duration-[0.3s]"
+                  />
+                  <button className=" absolute left-4 top-[18.5rem] z-10 transition-[top] ease-linear duration-[0.2s] h-12 w-44 text-base border-[0.01rem] border-black rounded-lg bg-buttonColor flex items-center justify-center gap-2 active:bg-clickColor group-hover:top-56">
+                    <Heart />
+                    <p className=" text-base font-[550]">Add to Whishlist</p>
+                  </button>
+                </div>
+                <div className=" px-4 space-y-3">
+                  <p className=" text-5xl">{name}</p>
+                  <p className=" text-2xl">₹{price}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
