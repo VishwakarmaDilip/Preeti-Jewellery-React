cea1c3586564f3887ca8d913c1496d4f25a96213
Dilip Vishwakarma <2106dilip@gmail.com>
2025-02-20 22:24:11 +0530

    fix: update file paths for favicon and hero image for correct loading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className=' relative flex justify-center h-full xs:h-screen w-screen xs:w-full'>
         <Navbar/>
         <div className=' h-full w-full overflow-hidden'> {/*hero section*/}
-            <img src="public/Image/HeroImage.jpg" alt="" loading='lazy' className=' w-full' />
+            <img src="../Image/HeroImage.jpg" alt="" loading='lazy' className=' w-full' />
         </div>
     </div>
   )
